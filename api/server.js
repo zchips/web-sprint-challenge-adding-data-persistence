24f8484993ce193d46a0b1b4a5d6777fdbdd66c8
zchips <81702433+zchips@users.noreply.github.com>
2021-11-05 09:15:23 -0600

    Server setup and PackageJSON w Dep

--- api/server.js
+++ api/server.js
@@ -1,1 +1,13 @@
 // build your server here and require it from index.js
+
+
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
+server.use((err, req, res, next)=>{
+    res.status(500).json({message: err.message})
+})
+
+module.exports = server
