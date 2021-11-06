419f4296c32a75646a5f7e1987b21fb71b39ff11
zchips <81702433+zchips@users.noreply.github.com>
2021-11-05 18:03:48 -0600

    :( codegrade bummed me out, but i like my test

--- data/seeds/04-project-resources.js
+++ data/seeds/04-project-resources.js
@@ -5,8 +5,8 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('project_resources').insert([
-        {project_resource_id: 1, project_id: 1, resource_id: 1},
-          {project_resource_id: 2, project_id: 2, resource_id: 2},
+        { project_id: 1, resource_id: 1},
+          { project_id: 2, resource_id: 2},
         ]);
       });
 };
